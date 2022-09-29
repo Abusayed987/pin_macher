35fd948cf7d85c534b414a821fce9161f8921dfb
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-29 23:21:39 +0600

    Verify match and show correct message

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 function genaretPin() {
-    const random = Math.round(Math.random() * 100000);
+    const random = Math.round(Math.random() * 10000);
     return random;
 }
 function getPin() {
     const pin = genaretPin();
     const pinString = pin + '';
-    console.log(pinString);
-    if (pinString.length === 5) {
+
+    if (pinString.length === 4) {
         return pin;
     } else {
         return getPin(); // jodi 4 na hoy tahole abar function colbe 
@@ -15,5 +15,33 @@ function getPin() {
 
 document.getElementById('btn_genaret_pin').addEventListener('click', function () {
     const displayPin = document.getElementById('display_pin');
-    displayPin.value = 'S-' + getPin();
+    displayPin.value = getPin();
+})
+document.getElementById('calculator').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    if (isNaN(number)) {
+        // console.log(number);
+    } else {
+        const typedNumberField = document.getElementById('typed_number');
+        const previousTypedNumber = typedNumberField.value;
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+})
+document.getElementById('varify_pin').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display_pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed_number');
+    const typedNumber = typedNumberField.value;
+
+    const successMessage = document.getElementById('success_message')
+    const failureMessage = document.getElementById('failure_message')
+    if (typedNumber === currentPin) {
+        successMessage.style.display = 'block'
+        failureMessage.style.display = 'none'
+    } else {
+        failureMessage.style.display = 'block'
+        successMessage.style.display = 'none'
+    }
 })
